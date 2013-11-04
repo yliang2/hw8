79a8e0bdaaa82f067313873058bc5a9ddfa603ff
yliang2 <yliang2@my.hpu.edu>
2013-11-04 02:57:26 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hw8::Application.config.secret_token = '15e74a1079dee6e072a6c00f486a479da46bf0695178d4c7cb3d45f79fe9dc81c09c38f412fcf7a4d3ae689286bda2db0386870a9d3e65fab682e40829136736'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Hw8::Application.routes.draw do
+  resources :movies
+  # map '/' to be a redirect to '/movies'
+  root :to => redirect('/movies')
+end
